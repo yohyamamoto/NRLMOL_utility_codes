12568815697fb433ca9b6f404d05d6a6e2c161a5
Yoh Yamamoto <yohyamamoto@gmail.com>
2023-03-15 17:47:55 -0600

    Create autoocc.dfa.sh

---
+++ input_file_scripts/autoocc.dfa.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# This script generate "OCCU" entry in the EVALUES file accordingly to SYMBOL.
+# May be useful in DFA calculations.
+
+eup=$(grep ELECTRON SYMBOL | awk '{print int($3)}')
+edn=$(grep ELECTRON SYMBOL | awk '{print int($4)}')
+##echo OCCU > EVALUES
+echo "OCCU"
+echo $eup  
+##for i in {1..'"$eup"'}; do echo "1 " ;done
+for ((i=1;i<=eup;i++)); do
+  echo -n "1 "
+done
+echo ""
+echo $edn
+#for i in {1..'"$edn"'}; do echo "1 " ;done
+for ((i=1;i<=edn;i++)); do
+  echo -n "1 "
+done
+echo ""
